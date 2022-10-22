5e33411a6006072b1376673b88bf47fda8151f71
Gildo Alves <104049719+16b8f3@users.noreply.github.com>
2022-10-22 09:44:01 -0300

    Tela de Login

---
+++ app_kivy_pizzaria.py
@@ -0,0 +1,21 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+
+class Gerente_das_telas(ScreenManager):
+    pass
+
+
+class Login(Screen):
+    pass
+
+
+class app_kivy_pizzaria(App):
+    def build(self):
+        return Gerente_das_telas()
+
+
+
+if __name__ == '__main__':
+    app_kivy_pizzaria().run()
