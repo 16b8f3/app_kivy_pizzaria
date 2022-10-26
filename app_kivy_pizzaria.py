8105c4abe626640ec73329970e0c9a317d86ee7a
Gildo Alves <104049719+16b8f3@users.noreply.github.com>
2022-10-26 13:07:20 -0300

    Fazendo o calculo e mostrando o valor total

--- app_kivy_pizzaria.py
+++ app_kivy_pizzaria.py
@@ -20,16 +20,25 @@ class Menu(Screen):
 
 
 class Pedido(Screen):
+    def pizzaTeste(self): # nome da pizza_TAMANHO
+        valor = 47.99 # valor da pizza
+        quantidade = (float(self.ids.teste.text)) # quantidade da pizza
+        a = valor * quantidade # valor total
+        total = "{:.2f}".format(a) # valor total limitado a duas casas decimais
+        self.ids.testeValor.text = str(total) # mudando o valor do total para o novo valor transformado em str
+        # a copia tem uma baita melhoria
+        # passar toda essa função para um arquivo a parte e importar aqui e usar so as funções
         
     def mais(self):
         print('aumenta o numero da pizza em 1')
         self.ids.teste.text = str(int(self.ids.teste.text) + 1)
+        self.pizzaTeste()
 
     def menos(self):
         if (int(self.ids.teste.text)) > 0:
             print('diminui o numero da pizza em 1')
             self.ids.teste.text = str(int(self.ids.teste.text) - 1)
-            
+            self.pizzaTeste()
 
 
 class Finalizar_Pedido(Screen):
